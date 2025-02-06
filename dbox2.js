94d714de47b21177b493f0f4ac1b72a2582387b4
tej2210030075 <dhruvnaidugona1004@gmail.com>
2025-02-06 11:34:40 +0530

    Forth commit

---
+++ dbox2.js
@@ -0,0 +1,11 @@
+var women = confirm("Are You Women");
+
+if(women){
+let name = promt("Please Enter Your Name");
+
+document.write("Hello Madam.: "+name+"Welcome to My Page");
+}
+else{
+    let name = prompt("Please Enter Your Name: ")
+    document.write("Hello Mr."+name+"Welcome to My Page")
+}
